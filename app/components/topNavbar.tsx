19882238451eada7aa0618b16c5fd43926e719e6
Daniel Shaw <danielshaw71@gmail.com>
2024-08-01 11:00:17 +0100

    Added styling to top navbar links

--- app/components/topNavbar.tsx
+++ app/components/topNavbar.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 
 const topNavbar = () => {
   return (
-    <nav className="bg-black border-b border-grey">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 border-b-grey">
+    <nav className="bg-black">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3">
-          <span className="self-center text-4xl font-semibold whitespace-nowrap font-main text-orange">DevUp</span>
+          <span className="self-center text-5xl font-semibold whitespace-nowrap font-main text-orange">DevUp</span>
         </a>
-        <div className="flex space-x-4">
-          <a href="#" className="text-white hover:text-orange font-sub text-xl transition duration-200 ease-in-out">Dashboard</a>
-          <span className="text-xl text-white">⫶</span>
-          <a href="#" className="text-white hover:text-orange font-sub text-xl transition duration-200 ease-in-out">Projects</a>
-          <span className="text-xl text-white">⫶</span>
-          <a href="#" className="text-white hover:text-orange font-sub text-xl transition duration-200 ease-in-out">Profile</a>
+        <div className="flex">
+          <a href="#" className="text-white hover:text-orange font-sub font-light text-xl transition-all duration-200 ease-in-out px-12 py-2 border border-white rounded-lg hover:rounded-3xl hover:border-orange">Dashboard</a>
+          <a href="#" className="text-white hover:text-orange font-sub font-light text-xl transition-all duration-200 ease-in-out px-12 py-2 border border-white rounded-lg hover:rounded-3xl hover:border-orange">Projects</a>
+          <a href="#" className="text-white hover:text-orange font-sub font-light text-xl transition-all duration-200 ease-in-out px-12 py-2 border border-white rounded-lg hover:rounded-3xl hover:border-orange">Profile</a>
         </div>
       </div>
     </nav>
