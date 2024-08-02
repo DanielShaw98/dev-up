3b31cad1e2e825aefde39f9460177dc4cf49b9a0
Daniel Shaw <danielshaw71@gmail.com>
2024-08-02 16:18:21 +0100

    Small design tweak

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const footer = () => {
   return (
-      <footer className="text-white font-body text-center py-6">
+      <footer className="text-white font-body text-center py-4">
         <p>© 2021 DevUp</p>
       </footer>
   )
