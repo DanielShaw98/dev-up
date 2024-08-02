e666089baa86eeaf01e7c445386305d0ddba2a37
Daniel Shaw <danielshaw71@gmail.com>
2024-08-02 16:27:39 +0100

    Removed debugging

--- app/api/auth/authOptions.ts
+++ app/api/auth/authOptions.ts
@@ -6,14 +6,9 @@ const githubClientId = process.env.GITHUB_ID;
 const githubClientSecret = process.env.GITHUB_SECRET;
 const googleClientId = process.env.GOOGLE_CLIENT_ID;
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const nextAuthSecret = process.env.NEXTAUTH_SECRET;
-
-console.log('Github Client ID:', githubClientId);
-console.log('Google Client ID:', googleClientId);
-console.log('NextAuth Secret:', nextAuthSecret);
 
 const authOptions: NextAuthOptions = {
-  secret: nextAuthSecret,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: githubClientId || "",
