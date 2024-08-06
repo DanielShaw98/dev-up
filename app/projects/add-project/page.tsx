8bb65c7b4d8aa52c505fb36d41851e411dcab687
Daniel Shaw <danielshaw71@gmail.com>
2024-08-06 14:15:20 +0100

    Added add project page

---
+++ app/projects/add-project/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddProject() {
+  return (
+    <div className="mx-32 my-8 h-screen">
+      <h1 className="text-3xl font-main text-center mb-6">Add Project</h1>
+    </div>
+  )
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Projects() {
   return (
     <div className="mx-32 my-8 h-screen">
       <h1 className="text-3xl font-main text-center mb-6">Projects</h1>
+      <div>
+        <Link href="/projects/add-project">Add Project</Link>
+      </div>
     </div>
   )
 }
